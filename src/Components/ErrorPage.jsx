e437a75dc11f5bc0c8821011a25255d852a596fc
johnson <ashirwadayirumala@gmail.com>
2024-08-06 00:09:53 +0530

    six

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import notfound from "/assets/notfound.png";
 
 const ErrorPage = () => {
   return (
     <div className="h-screen flex items-center justify-center flex-col">
       <div>
-        <img className="object-cover h-96" src={notfound} alt="hello" />
+        <img
+          className="object-cover h-96"
+          src="/assets/notfound.png"
+          alt="hello"
+        />
       </div>
       <div className="text-3xl">Sorry this page does not exist</div>
       <h2>404 </h2>
